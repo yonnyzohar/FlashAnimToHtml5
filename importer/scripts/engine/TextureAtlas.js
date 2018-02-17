14d103123c8c60fc3bdbbe46a99d17f20260cc1c
Yonny Zohar <yonny@scene53.com>
2018-02-17 23:54:47 +0200

    Supporting multiple atlases

--- importer/scripts/engine/TextureAtlas.js
+++ importer/scripts/engine/TextureAtlas.js
@@ -1,40 +1,62 @@
-class TextureAtlas {
+var TextureAtlas = {
+    dict: {},
+    atlas: [],
 
-    static init(_loadCompleteFnctn, _progressFnctn, _assetsPath) {
+
+    init: function (_loadCompleteFnctn, _progressFnctn, _assetsPath) {
         PIXI.loader.add(_assetsPath).load(function () {
             trace("ATLAS LOADED " + _assetsPath);
-            TextureAtlas.atlas = PIXI.loader.resources[_assetsPath].textures;
+            TextureAtlas.atlas.push(PIXI.loader.resources[_assetsPath].textures);
             _loadCompleteFnctn();
         }).on('progress', function (e) {
             _progressFnctn(e.progress)
         });
 
-    }
+    },
 
-    static createFrame(itemName) {
-        //trace(itemName);
-        if (!TextureAtlas.atlas[itemName]) {
-            itemName = itemName + "0000";
+
+
+    createFrame: function (itemName) {
+        trace(itemName);
+
+        var found = false;
+        var img = null;
+
+        for (var i = 0; i < TextureAtlas.atlas.length; i++) {
+            var a = TextureAtlas.atlas[i];
+            var _itemName = itemName;
+            if (!a[_itemName]) {
+                _itemName = _itemName + "0000";
+            }
+
+            if (a[_itemName]) {
+                img = new PIXI.Sprite(a[_itemName]);
+                break;
+            }
+
         }
-        var image = new PIXI.Sprite(TextureAtlas.atlas[itemName]);
-        if (image == null) {
-            trace("could not create " + itemName);
 
+        if (img == null) {
+            trace("COULD NOT FIND " + itemName);
         }
-        return image;
-    }
 
-    static getNumOfFrames(_framePrefix) {
+        return img;
+    },
+
+    getNumOfFrames: function (_framePrefix) {
         var num = 0;
-        for (var k in TextureAtlas.atlas) {
-            if (k.indexOf(_framePrefix) != -1) {
-                num++;
+        for (var i = 0; i < TextureAtlas.atlas.length; i++) {
+            var a = TextureAtlas.atlas[i];
+            for (var k in a) {
+                if (k.indexOf(_framePrefix) != -1) {
+                    num++;
+                }
             }
         }
         return num;
-    }
+    },
 
-    static createMovieClip(_framePrefix) {
+    createMovieClip: function (_framePrefix) {
         var frames = [];
         var numFrames = TextureAtlas.getNumOfFrames(_framePrefix);
         trace(numFrames + " in " + _framePrefix);
@@ -52,6 +74,3 @@ class TextureAtlas {
         return mc;
     }
 }
-
-TextureAtlas.dict = {};
-TextureAtlas.atlas = null;
